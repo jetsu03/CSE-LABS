035820ea0dd18f41fd3006d3acfcc80abc3a1515
Neha Honniganur <134662930+jetsu03@users.noreply.github.com>
2025-05-17 20:12:18 +0530

    Create BubbleSort.c

---
+++ C Lab/BubbleSort.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+void bubbleSort(int arr[], int n) {
+    int i, j, temp;
+    for (i = 0; i < n - 1; i++) { //Number of passes
+        for (j = 0; j < n - i - 1; j++) { //Index in the array
+            if (arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+int main() {
+    int n, i;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter %d numbers:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    bubbleSort(arr, n);
+    printf("Sorted array:\n");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
